ea449f76fa3a8fbc74c8e41f55180c802076a172
teslatickles <hunterhartline@gmail.com>
2021-01-20 00:08:56 -0600

    add indentation on ReactLoading and update gitignore to exclude scrap file for development

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,13 +3,13 @@ import ReactLoading from "react-loading";
 import "../styles/loading.css";
 
 const Loading = ({ type, color }) => (
-  <ReactLoading
-    type="cylon"
-    color="#ff3f34"
-    height={150}
-    width={150}
-    className="loading-cylon"
-  />
+    <ReactLoading
+        type="cylon"
+        color="#ff3f34"
+        height={150}
+        width={150}
+        className="loading-cylon"
+    />
 );
 
 export default Loading;
