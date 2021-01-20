131c4cf5bc4fc5900ed8db6f307e78ce24e15691
teslatickles <hunterhartline@gmail.com>
2021-01-19 23:52:13 -0600

    fix averaged lap times logic to divide by number of records in database - move global data var to constructor

--- src/components/LapTimes.jsx
+++ src/components/LapTimes.jsx
@@ -3,95 +3,79 @@ import ReactTable from "react-table";
 import "react-table/react-table.css";
 import "../styles/laptimes.css";
 
-let tableData = [];
-
 class LapTimes extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      laps: [],
-      data: []
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.tableData = [];
+        this.state = {
+            laps: [],
+            data: []
+        };
+    }
 
-  //   shouldComponentUpdate(nextProps) {
-  //     if (this.props.value !== nextProps.value) {
-  //       return true;
-  //     } else {
-  //       return false;
-  //     }
-  //   }
+    componentDidMount() {
+        this.setState({
+            data: this.tableData
+        });
+    }
 
-  componentDidMount() {
-    this.setState({
-      data: tableData
-    });
-  }
+    render() {
+        const { lapTimes } = this.props;
+        let lapTotalTimes = 0.0;
+        let avgTime = 0.0;
 
-  render() {
-    const { pCount, lapTimes } = this.props;
-    lapTimes.map(
-      (current, i) =>
-        (tableData[i] = {
-          [`count`]: i + 1,
-          [`time`]: current / 1000
-        })
-    );
-    // console.log(this.props.lapTimes);
-    const avgTime = lapTimes.reduce((sum, lapTime) => {
-      sum += lapTime;
-      sum = sum / pCount;
-      return sum;
-    }, 0);
-    // console.log(pCount, avgTime);
-    const columns = [
-      {
-        getFooterProps: (state, rowInfo, column) => {
-          return {
-            style: {
-              color: rowInfo.row.name === "Total: 1" ? "red" : null
+        lapTimes.map(
+            (current, i) =>
+            (this.tableData[i] = {
+                [`count`]: i + 1,
+                [`time`]: (current / 1000).toFixed(2)
+            })
+        );
+
+        lapTimes.forEach(lapTime => lapTotalTimes += lapTime);
+        avgTime = (lapTotalTimes / lapTimes.length) / 1000;
+
+        const columns = [
+            {
+                Header: `${this.props.date}`,
+                columns: [
+                    {
+                        Header: "Session",
+                        accessor: "count",
+                        Footer: (
+                            <span>
+                                <strong>Total: {this.tableData.length}</strong>
+                            </span>
+                        )
+                    },
+                    {
+                        Header: "Time",
+                        accessor: "time",
+                        id: d => d.time,
+                        Footer: (
+                            <span>
+                                <strong>Average: {(avgTime).toFixed(2)}</strong>
+                            </span>
+                        )
+                    }
+                ]
             }
-          };
-        },
-        Header: `Restroom Data for ${this.props.date}`,
-        columns: [
-          {
-            Header: "Session",
-            accessor: "count",
-            Footer: (
-              <span>
-                <strong>Total: {pCount}</strong>
-              </span>
-            )
-          },
-          {
-            Header: "Time",
-            accessor: "time",
-            id: d => d.time,
-            Footer: (
-              <span>
-                <strong>Average: {(avgTime / 1000).toFixed(2)}</strong>
-              </span>
-            )
-          }
-        ]
-      }
-    ];
-    console.log(this.state.data);
-    return (
-      <div>
-        <div id="scroll-tbl">
-          <ReactTable
-            className="-striped -highlight"
-            noDataText="oh no, cannot find data"
-            defaultPageSize={5}
-            data={this.state.data}
-            columns={columns}
-          />
-        </div>
-      </div>
-    );
-  }
+        ];
+
+        return (
+            <div>
+                <div id="scroll-tbl">
+                    <ReactTable
+                        className="-striped -highlight"
+                        noDataText="oh no, cannot find data"
+                        defaultPageSize={10}
+                        data={this.state.data}
+                        columns={columns}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default LapTimes;
